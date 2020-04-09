54a8eb6c0c1e2bbf9013afb1845b6c35a42f85d7
andryxins <andryxins@icloud.com>
2020-04-09 14:06:14 +0300

    Updates

---
+++ precache-manifest.deeb55ab526563d7a51d8f48d954a3d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af7833e1eec7b0a3c9a4f7f1daf06aa",
+    "url": "/goit-react-hw/index.html"
+  },
+  {
+    "revision": "e7c6104128abbab86b38",
+    "url": "/goit-react-hw/static/css/main.173fec27.chunk.css"
+  },
+  {
+    "revision": "10039ae213e01775b802",
+    "url": "/goit-react-hw/static/js/2.2faea33c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw/static/js/2.2faea33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c6104128abbab86b38",
+    "url": "/goit-react-hw/static/js/main.8a098dec.chunk.js"
+  },
+  {
+    "revision": "f531c8eedc1321c45844",
+    "url": "/goit-react-hw/static/js/runtime-main.e1b43269.js"
+  }
+]);
